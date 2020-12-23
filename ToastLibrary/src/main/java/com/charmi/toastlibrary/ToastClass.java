b64eb45e2a496521817d95922f020ede9b6be7cc
charmi <charmi.solanki@loylty.com>
2020-12-23 14:57:58 +0530

    updated code

--- ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastClass.java
+++ ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastClass.java
@@ -3,7 +3,10 @@ package com.charmi.toastlibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-public class ToastClass {
+public  class ToastClass {
 
+    public static void showToast(Context ctx){
+        Toast.makeText(ctx, "Hello from toast library...", Toast.LENGTH_LONG).show();
 
+    }
 }
--- app/src/main/java/com/charmi/demolibrary/MainActivity.java
+++ app/src/main/java/com/charmi/demolibrary/MainActivity.java
@@ -12,12 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        showToast();
     }
 
-    public void showToast(){
-        Toast.makeText(this, "Hello from toast library...", Toast.LENGTH_LONG).show();
 
-    }
 }
