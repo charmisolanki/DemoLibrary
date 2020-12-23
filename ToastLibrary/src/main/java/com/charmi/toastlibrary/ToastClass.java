c012186826786820406a1084c519ddba7ed034a3
charmi <charmi.solanki@loylty.com>
2020-12-23 14:43:47 +0530

    updated code

--- ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastClass.java
+++ ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastClass.java
@@ -5,8 +5,5 @@ import android.widget.Toast;
 
 public class ToastClass {
 
-    public void showToast(Context context){
-        Toast.makeText(context, "Hello from toast library...", Toast.LENGTH_LONG).show();
 
-    }
 }
--- app/src/main/java/com/charmi/demolibrary/MainActivity.java
+++ app/src/main/java/com/charmi/demolibrary/MainActivity.java
@@ -2,7 +2,9 @@ package com.charmi.demolibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showToast();
+    }
+
+    public void showToast(){
+        Toast.makeText(this, "Hello from toast library...", Toast.LENGTH_LONG).show();
+
     }
 }
