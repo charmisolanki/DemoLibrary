9036ba3191bf6f854597a67162c19d48765c5e9b
charmi <charmi.solanki@loylty.com>
2021-03-01 10:09:12 +0530

    added cust id private field in property file

--- ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastActivity.java
+++ ToastLibrary/src/main/java/com/charmi/toastlibrary/ToastActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import static com.charmi.toastlibrary.BuildConfig.CUSTOMER_ID;
+
 public class ToastActivity extends AppCompatActivity {
 
     private String toast_data = null;
@@ -15,7 +17,6 @@ public class ToastActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
 
-//        BuildConfig.CLIENT_ID = "111";
 
         if(getIntent() != null && getIntent().getExtras() != null){
             Bundle bundle = getIntent().getBundleExtra("my_bundle");
@@ -29,6 +30,6 @@ public class ToastActivity extends AppCompatActivity {
 
 
     public void demoFunc(String data){
-        Toast.makeText(this ,data, Toast.LENGTH_LONG).show();
+        Toast.makeText(this ,data +"new cust_id= " + CUSTOMER_ID, Toast.LENGTH_LONG).show();
     }
 }
