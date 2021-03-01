4e6e5775ab2b070fdd9ecdaeeb4078fa69567758
charmi <charmi.solanki@loylty.com>
2021-03-01 09:48:50 +0530

    added cust id private field in property file

--- app/src/main/java/com/charmi/demolibrary/MainActivity.java
+++ app/src/main/java/com/charmi/demolibrary/MainActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvShowCustId;
+    private String cust_id = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvShowCustId = findViewById(R.id.tvShowCustId);
+
+        if (getIntent() != null)
+            cust_id = getIntent().getBundleExtra("cust_id").toString();
+
+        tvShowCustId.setText(BuildConfig.CUSTOMER_ID + " new cust_id= " + cust_id);
+
     }
 
 
